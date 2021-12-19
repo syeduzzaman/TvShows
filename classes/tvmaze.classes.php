51bdf5da448cd6daadb52b5f0c3d58aa6e9b8bec
Mohammad Syeduzzaman <42605892+syeduzzaman@users.noreply.github.com>
2021-12-19 17:07:34 +0100

    curl_close Added
    curl_close Added to close cURL resource, and free up system resources.

--- classes/tvmaze.classes.php
+++ classes/tvmaze.classes.php
@@ -8,7 +8,7 @@ class Tvmaze {
         $api_link = "https://api.tvmaze.com/search/shows?q=".$searchMovie;
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL,$api_link);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+        //curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $responseJson = curl_exec($curl);
         $responseArray = json_decode($responseJson, true);
@@ -18,17 +18,18 @@ class Tvmaze {
                 $movieName = $respons["show"]["name"];
                 //Only the movies with exact name match(non-case-sensitive) is considered
                 if(!empty($movieName) && strtolower($movieName) == strtolower($searchMovie)){
-                    $resultArray[$movieName] = $respons;
+                    $resultArray[] = $respons;
                 }
             }
         }
 
         $resultArray = count($resultArray) > 0 ? $resultArray : $resultArray = array("Error"=>"No Movie Found!");
+        $resultJson = json_encode($resultArray); 
+        curl_close($curl);
 
-        $resultJson = json_encode($resultArray);
         return $resultJson;
 
-    }
+     }
 
 }
 
